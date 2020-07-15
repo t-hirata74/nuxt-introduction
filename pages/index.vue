578a7f78452df2d7a77f61db456035dc145074aa
Tomoaki Hirata <t.hirata.0463@gmail.com>
2020-07-15 16:02:39 +0900

    section8 finish

--- pages/index.vue
+++ pages/index.vue
@@ -36,12 +36,27 @@
     <div>
       <img src="~/assets/cat.jpg">
     </div>
+
+    <div>
+      <!-- <p>{{ $store.state.message }}</p> -->
+      <!-- <p>{{ $store.state.hello.message }}</p> -->
+      <p>{{ $store.state.hello.message }}</p>
+
+      <!-- <button v-on:click="$store.commit('updateMessage')">Update</button> -->
+      <!-- <button v-on:click="$store.commit('updateMessage', 'Commit with payload')">Update</button> -->
+      <!-- <button v-on:click="$store.dispatch('updateMessageAction')">Dispatch</button> -->
+      <!-- <button v-on:click="$store.dispatch('updateMessageAction', 'Dispatch with payload')">Dispatch</button> -->
+      <!-- <button v-on:click="$store.dispatch('hello/updateMessageAction', 'Dispatch with payload')">Dispatch</button> -->
+      <button v-on:click="$store.dispatch('hello/updateMessageAction', 'Dispatch with payload')">Dispatch</button>
+
+    </div>
+
   </div>
 </template>
 
 <script>
 const axios = require('axios')
-let url = 'https://jsonplaceholder.typicode.com/usersxxxx'
+let url = 'https://jsonplaceholder.typicode.com/users'
 
 export default {
   //asyncDataはnuxtのメソッド
---
+++ store/hello.js
@@ -0,0 +1,16 @@
+// module mode
+export const state = () => ({
+  message: 'Hello Vuex!'
+})
+
+export const mutations = {
+  updateMessage: function(state, payload) {
+    state.message = payload
+  }
+}
+
+export const actions = {
+  updateMessageAction(context, payload) {
+    context.commit('updateMessage', payload)
+  }
+}
