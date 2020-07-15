4895c225e17eed6f5a4ba5e7d9dcab667df8b664
Tomoaki Hirata <t.hirata.0463@gmail.com>
2020-07-15 12:11:44 +0900

    section6 finish

--- pages/index.vue
+++ pages/index.vue
@@ -24,11 +24,35 @@
         </a>
       </div>
     </div>
+    <div>
+      <!-- {{ users[0].id }}, {{ users[0].name }} -->
+      <ul>
+        <li v-for="user in users" :key="user.id">
+          {{ user.id }}, {{ user.name }}, {{ user.company.name }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+const axios = require('axios')
+let url = 'https://jsonplaceholder.typicode.com/usersxxxx'
+
+export default {
+  //asyncDataはnuxtのメソッド
+  asyncData({ params, error }) {
+    return axios.get(url)
+    .then((res => {
+      return {users: res.data}
+    }))
+    .catch(( e => {
+      console.log(e.response.status)
+      console.log(error)
+      // error({ users: e.response.status, message: 'Error!'})
+    }))
+  }
+}
 </script>
 
 <style>
